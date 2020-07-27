8b24ce717f879c146a2a0620451dc5337e2ffecc
Amyre Polk <myremyre101@icloud.com>
2020-07-27 01:41:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student
+  attr_accessor :knowledge
 
+    def initialize
+      @knowledge = []
+    end
+
+    def learn(knowledge)
+      @knowledge << knowledge
+    end
+
+    def knowledge
+      @knowledge
+    end
 end
